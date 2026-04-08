5becd8bfd0d0aa222bd30254d11e8e5825920e51
AjeetSingh21 <ajeet.singh77@outlook.com>
2026-04-08 14:33:44 +0530

    UC2 added by ajeet

--- app.java
+++ app.java
@@ -4,9 +4,22 @@ public class app {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        List<String> trainConsist = new ArrayList<>();
+        List<String> passengerBogies = new ArrayList<>();
 
-        System.out.println("Train consist initialized.");
-        System.out.println("Initial bogie count: " + trainConsist.size());
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Passenger Bogies after addition: " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("After removing AC Chair: " + passengerBogies);
+
+        boolean exists = passengerBogies.contains("Sleeper");
+
+        System.out.println("Does Sleeper exist? " + exists);
+
+        System.out.println("Final Passenger Bogies: " + passengerBogies);
     }
 }
