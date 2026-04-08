daf85faaf2ccc08bf8035d631997a801b479a2bc
AjeetSingh21 <ajeet.singh77@outlook.com>
2026-04-08 14:49:24 +0530

    UC4 added by ajeet

--- app.java
+++ app.java
@@ -1,17 +1,36 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
 
-public class app {
+
+public class UseCase4TrainConsistMgmt {
     public static void main(String[] args) {
-        System.out.println("=== Train Consist Management App ===");
 
-        Set<String> bogieIds = new HashSet<>();
+        System.out.println("=====================================");
+        System.out.println("UC4 - Maintain Ordered Bogie Consist");
+        System.out.println("=====================================\n");
 
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
-        bogieIds.add("BG103");
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
+        List<String> trainConsist = new LinkedList<>();
 
-        System.out.println("Unique Bogie IDs: " + bogieIds);
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
+
+        System.out.println("Initial Train Consist:");
+        System.out.println(trainConsist + "\n");
+
+        trainConsist.add(2, "Pantry Car");
+
+        System.out.println("After Inserting 'Pantry Car' at position 2:");
+        System.out.println(trainConsist + "\n");
+
+        ((LinkedList<String>) trainConsist).removeFirst();
+        ((LinkedList<String>) trainConsist).removeLast();
+
+        System.out.println("After Removing First and Last Bogie:");
+        System.out.println(trainConsist + "\n");
+
+        System.out.println("UC4 ordered consist operations completed...");
     }
 }
